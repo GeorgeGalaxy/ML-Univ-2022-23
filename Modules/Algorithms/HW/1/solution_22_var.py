05a112abe473921f0b3eb4fc2b7bc5d9e88ce322
Георгий <georg1506@mail.ru>
2022-11-23 13:17:23 +0300

    feat(univ): add sorting + conclusions

--- Modules/Algorithms/HW/1/solution_22_var.py
+++ Modules/Algorithms/HW/1/solution_22_var.py
@@ -1,3 +1,12 @@
+# ----------------------------------------------------
+# Номер студенческого оканчивается на 82 => 82%30 = 22
+# Вариант 22: дек и простой выбор
+#
+# Сложность алгоритма по времени O(n^2)
+# Сложность алгоритма по памяти O(1)
+# ----------------------------------------------------
+
+
 class Item:
     def __init__(self, value=None, next=None, prev=None):
         self.value = value
@@ -48,25 +57,59 @@ class Deque:
                   f'(prev = {el.prev}, next = {el.next})')
 
 item1 = Item(1)
-item2 = Item(2)
-item3 = Item(3)
+item2 = Item(4)
+item3 = Item(2)
 item4 = Item(8)
+item5 = Item(10)
 
 main_deq = Deque()
 main_deq.push_last(item1)
 main_deq.push_first(item2)
 main_deq.push_last(item3)
 main_deq.push_last(item4)
+main_deq.push_last(item5)
 
 main_deq.print()
-print()
+print('-'*20)
 
-main_deq.remove_last()
+def simple_selection_sort(deque):
 
+    N = deque.get_size()
+
+    for i in range(N - 1):
+
+        min_value = deque.items[i].value
+        min_index = i
+
+        for j in range(i + 1, N):
+            if min_value > deque.items[j].value:
+
+                min_value = deque.items[j].value
+                min_index = j
+
+        if min_index != i:
+
+            temp_value = deque.items[i].value
+
+            deque.items[i].value = deque.items[min_index].value
+            deque.items[i].prev = deque.items[min_index - 1].prev
+
+            deque.items[min_index].value = temp_value
+            deque.items[min_index].next = deque.items[min_index + 1].value
+            deque.items[min_index].prev = deque.items[min_index - 1].value
+
+            deque.items[i].next = deque.items[i + 1].value
+
+            # Изменение соседей
+            if i != 0:
+                deque.items[i - 1].next = deque.items[i].value
+            else:
+                pass
+            deque.items[i + 1].prev = deque.items[i].value
+            deque.items[min_index - 1].next = deque.items[min_index].value
+            deque.items[min_index + 1].prev = deque.items[min_index].value
+
+    return deque
+
+main_deq = simple_selection_sort(main_deq)
 main_deq.print()
-print()
-
-main_deq.remove_first()
-
-main_deq.print()
-print()
